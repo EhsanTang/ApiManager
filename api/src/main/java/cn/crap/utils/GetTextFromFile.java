e70fd19536bd6ee448ee6360150a0dcd53994437
IhsanTang <IhsanTang@163.com>
2016-07-05 22:08:07 +0800

    docx 文件上传失败，布局修改

--- api/src/main/java/cn/crap/utils/GetTextFromFile.java
+++ api/src/main/java/cn/crap/utils/GetTextFromFile.java
@@ -6,25 +6,16 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Iterator;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.poi.POITextExtractor;
 import org.apache.poi.extractor.ExtractorFactory;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.usermodel.Range;
+import org.apache.poi.hwpf.extractor.WordExtractor;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.hwpf.OldWordFileFormatException;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.xmlbeans.XmlException;
 
 public class GetTextFromFile {
@@ -38,7 +29,7 @@ public class GetTextFromFile {
 			return getTextFromWord(filePath);
 		} else if (filePath.toUpperCase().endsWith(".TXT")) {
 			return getTextFromTxt(filePath);
-		} else if (filePath.toUpperCase().endsWith(".XLS") || filePath.toUpperCase().endsWith(".XLSX")){
+		} else if (filePath.toUpperCase().endsWith(".XLS") || filePath.toUpperCase().endsWith(".XLSX")) {
 			return getTextFromExcel(filePath);
 		}
 		return "";
@@ -80,16 +71,43 @@ public class GetTextFromFile {
 
 	private static String getTextFromWord(String worldFilePth) throws Exception {
 		FileInputStream fis = null;
+		WordExtractor extractor2003 = null;
+		XWPFWordExtractor extractor2007 = null;
 		try {
+
 			fis = new FileInputStream(worldFilePth);
-			HWPFDocument doc = new HWPFDocument(fis);
-			Range rang = doc.getRange();
-			return rang.text();
+			if (worldFilePth.toUpperCase().endsWith(".DOC")) {
+				extractor2003 = new WordExtractor(fis);
+				return extractor2003.getText();
+			} else {
+				XWPFDocument doc = new XWPFDocument(fis);
+				extractor2007 = new XWPFWordExtractor(doc);
+				return extractor2007.getText();
+			}
+
 		} catch (Exception e) {
 			throw e;
 		} finally {
-			if (fis != null) {
-				fis.close();
+			try {
+				if (fis != null) {
+					fis.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				if (extractor2003 != null) {
+					extractor2003.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				if (extractor2007 != null) {
+					extractor2007.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
 	}
@@ -125,10 +143,11 @@ public class GetTextFromFile {
 		return result.toString();
 
 	}
-	
-	private static String getTextFromExcel(String fileName) throws InvalidFormatException, IOException, OpenXML4JException, XmlException{ 
+
+	private static String getTextFromExcel(String fileName)
+			throws InvalidFormatException, IOException, OpenXML4JException, XmlException {
 		File inputFile = new File(fileName);
-		POITextExtractor extractor = ExtractorFactory.createExtractor(inputFile);  
-        return extractor.getText();
-    }  
+		POITextExtractor extractor = ExtractorFactory.createExtractor(inputFile);
+		return extractor.getText();
+	}
 }

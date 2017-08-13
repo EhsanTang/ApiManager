c67331935374975438b92aabd78feac69bfec2f8
IhsanTang <IhsanTang@163.com>
2017-08-13 14:14:28 +0800

    mybatis

---
+++ api/src/test/java/unitTest/MybatisGenerator.java
@@ -0,0 +1,19 @@
+package unitTest;
+
+import org.junit.Test;
+
+
+public class MybatisGenerator {
+    public static void main(String args[]) throws Exception {
+        System.out.println("-----------");
+        String f = null;
+        try {
+            f = MybatisGenerator.class.getResource("dao-generator.xml").getPath();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("------------"+f);
+        GenerateCode.generate(f);
+    }
+
+}

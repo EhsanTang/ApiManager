7affbf53e4f838ce372d04aaa6d39d58b2d7999d
IhsanTang <IhsanTang@163.com>
2018-05-05 18:55:19 +0800

    删除apidev

--- api/CrapApi.V7.9.sql
+++ api/CrapApi.V7.9.sql
@@ -37,8 +37,7 @@ ADD COLUMN `version` INT NOT NULL DEFAULT 0 AFTER `templateId`;
 
 INSERT INTO `setting` (`id`, `mkey`, `value`, `remark`, `type`, `canDelete`, `sequence`) VALUES ('fpmbdc8b-9cd8-4839-b38a-898343435462', 'INDEX_PAGE', 'index.do#/web/article/list/web/PAGE/%E6%8A%80%E6%9C%AF%E6%96%87%E6%A1%A3/NULL', '前端首页：只能以index.do、font/ 开头的url', 'INDEXPAGE', '0', '66');
 
-ALTER TABLE `user` 
-CHANGE COLUMN `loginType` `loginType` INT(11) NOT NULL DEFAULT '0' COMMENT '0：账号登陆，1：github登陆，2：码云' ;
+ALTER TABLE `user` CHANGE COLUMN `loginType` `loginType` INT(11) NOT NULL DEFAULT '0' COMMENT '0：账号登陆，1：github登陆，2：码云' ;
 
 update `user` set loginType=1 where thirdlyId like 'gitHub%';
 INSERT INTO `setting` (`id`, `mkey`, `value`, `remark`, `type`, `canDelete`, `sequence`) VALUES ('fff-1111-d4839-b38a-898343435462', 'ANONYMOUS_COMMENT', 'true', '是否允许匿名评论, true:允许', 'TEXT', '0', '100');
@@ -52,27 +51,26 @@ ADD COLUMN `userName` VARCHAR(50) NOT NULL COMMENT '匿名' AFTER `avatarUrl`;
 
 update comment set userName='匿名' where userName='' or userName is null;
 
-ALTER TABLE user`
-ADD COLUMN `passwordSalt` VARCHAR(20) NULL COMMENT '密码MD5盐' AFTER `thirdlyId`;
+ALTER TABLE user ADD COLUMN `passwordSalt` VARCHAR(20) NULL COMMENT '密码MD5盐' AFTER `thirdlyId`;
 
 
-ALTER TABLE `apidev`.`setting`
+ALTER TABLE `setting`
 CHANGE COLUMN `remark` `remark` VARCHAR(500) NULL DEFAULT NULL ;
 
 --- 未执行，线上
-ALTER TABLE `apidev`.`error`
+ALTER TABLE `error`
 CHANGE COLUMN `moduleId` `projectId` VARCHAR(50) NOT NULL ;
 
-ALTER TABLE `apidev`.`setting`
+ALTER TABLE `setting`
 ADD COLUMN `show` TINYINT(4) NOT NULL DEFAULT 1 COMMENT '是否在前端显示，1：是，0：否' AFTER `sequence`;
 
-ALTER TABLE `apidev`.`article`
+ALTER TABLE `article`
 ADD COLUMN `projectId` VARCHAR(50) NOT NULL DEFAULT '' AFTER `markdown`;
 
-ALTER TABLE `apidev`.`interface`
+ALTER TABLE `interface`
 ADD COLUMN `projectId` VARCHAR(50) NOT NULL DEFAULT '' AFTER `isTemplate`;
 
-ALTER TABLE `apidev`.`source`
+ALTER TABLE `source`
 ADD COLUMN `projectId` VARCHAR(50) NOT NULL DEFAULT '' AFTER `filePath`;
 
 CREATE TABLE `hot_search` (
@@ -86,14 +84,14 @@ CREATE TABLE `hot_search` (
   KEY `index_times` (`times`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-ALTER TABLE `apidev`.`module`
+ALTER TABLE `module`
 ADD COLUMN `category` VARCHAR(200) NOT NULL DEFAULT '' COMMENT '文章分类，多个分类以逗号分割，每个分类最多10个字';
 
 update article set mkey=null where  type!='PAGE';
 
 update article set status=100 where  type='PAGE'
 
-ALTER TABLE `apidev`.`interface`
+ALTER TABLE `interface`
 ADD COLUMN `contentType` VARCHAR(45) NOT NULL DEFAULT 'application/json' COMMENT '接口返回contentType' AFTER `projectId`;
 
 

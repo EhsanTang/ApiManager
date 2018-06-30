1f637cd8b3877f93c031cd1bad8fa090e8edb963
IhsanTang <IhsanTang@163.com>
2018-07-01 01:33:59 +0800

    解决图形验证码字体乱码问题

---
+++ api/src/main/java/cn/crap/framework/InitSystem.java
@@ -0,0 +1,74 @@
+package cn.crap.framework;
+
+import cn.crap.adapter.SettingAdapter;
+import cn.crap.dto.SettingDto;
+import cn.crap.enumer.SettingEnum;
+import cn.crap.model.mybatis.Setting;
+import cn.crap.service.mybatis.SettingService;
+import cn.crap.service.tool.SettingCache;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Ehsan
+ * @date 2018/6/30 23:07
+ */
+@Service
+public class InitSystem {
+    @Resource
+    private SettingCache settingCache;
+    private SettingService settingService;
+
+    @Resource
+    public void InitSystem(SettingService settingService){
+        this.settingService = settingService;
+        init();
+    }
+
+    public void init(){
+        System.out.println("初始化系统数据中....");
+        initSetting();
+    }
+    /**
+     * 初始化系统配置
+     */
+    public void initSetting(){
+        List<SettingDto> settingDtos = settingCache.getAll();
+        Map<String, SettingDto> settingDtoMap = new HashMap<>();
+        for (SettingDto settingDto : settingDtos){
+            settingDtoMap.put(settingDto.getKey(), settingDto);
+        }
+
+        // 新增加且没有写入数据库的配置，并储存至数据库
+        for (SettingEnum settingEnum : SettingEnum.values()){
+            try {
+                String key = settingEnum.getKey();
+                if (!settingDtoMap.containsKey(key)) {
+                    settingService.insert(settingEnum.getSetting());
+                    settingCache.del(key);
+                    continue;
+                }
+
+                SettingDto settingDto = settingDtoMap.get(key);
+                boolean needUpdate = ((!settingDto.getStatus().equals(settingEnum.getStatus())) ||
+                        (!settingDto.getType().equals(settingEnum.getType())));
+                if (needUpdate){
+                    Setting setting = SettingAdapter.getModel(settingDto);
+                    setting.setStatus(settingEnum.getStatus());
+                    setting.setType(settingEnum.getType());
+                    setting.setCanDelete(null);
+
+                    settingService.update(setting);
+                    settingCache.del(key);
+                    continue;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}

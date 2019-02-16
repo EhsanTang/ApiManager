3fca2806b95ab76eb8571ffcc841ce3aa6568ebe
IhsanTang <IhsanTang@163.com>
2019-02-16 21:52:49 +0800

    添加错误测试接口

--- api/src/main/java/cn/crap/controller/visitor/ExampleController.java
+++ api/src/main/java/cn/crap/controller/visitor/ExampleController.java
@@ -1,15 +1,16 @@
 package cn.crap.controller.visitor;
 
+import cn.crap.dto.XmlParamsDto;
+import cn.crap.framework.JsonResult;
+import cn.crap.framework.base.BaseController;
 import cn.crap.model.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import cn.crap.dto.XmlParamsDto;
-import cn.crap.framework.JsonResult;
-import cn.crap.framework.MyException;
-import cn.crap.framework.base.BaseController;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 测试接口
@@ -20,27 +21,33 @@ import cn.crap.framework.base.BaseController;
 public class ExampleController extends BaseController{
 	@RequestMapping("/visitor/example/json.do")
 	@ResponseBody
-	public JsonResult json(@RequestBody Article article) throws MyException{
+	public JsonResult json(@RequestBody Article article){
 		return new JsonResult(1, article);
 	}
 	@RequestMapping("/visitor/example/xml.do")
 	@ResponseBody
-	public JsonResult xml(@RequestBody XmlParamsDto users) throws MyException{
+	public JsonResult xml(@RequestBody XmlParamsDto users){
 		return new JsonResult(1, users);
 	}
 	
 	@RequestMapping("/visitor/example/body.do")
 	@ResponseBody
-	public JsonResult body(@RequestBody String body) throws MyException{
+	public JsonResult body(@RequestBody String body){
 		log.warn("--visitor/example/body.do:" + body);
 		return new JsonResult(1, body);
 	}
 	
 	@RequestMapping("/visitor/example/post.do")
 	@ResponseBody
-	public JsonResult post(@RequestBody String json) throws MyException{
-		System.out.println(json);
+	public JsonResult post(@RequestBody String json){
 		return new JsonResult(1, json);
+	}
+
+	@RequestMapping("/visitor/example/error.do")
+	@ResponseBody
+	public JsonResult error(@RequestParam(defaultValue = "200") Integer errorCode, HttpServletResponse response){
+		response.setStatus(302);
+		return new JsonResult(1, errorCode);
 	}
 
 	

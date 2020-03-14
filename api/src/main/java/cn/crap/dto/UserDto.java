8a520a5030f9a579a4338f9ee825d1bfaa75e5e5
IhsanTang <IhsanTang@163.com>
2020-03-15 03:14:25 +0800

    用户打标

--- api/src/main/java/cn/crap/dto/UserDto.java
+++ api/src/main/java/cn/crap/dto/UserDto.java
@@ -1,11 +1,15 @@
 package cn.crap.dto;
 
 import cn.crap.enu.UserType;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
 
-public class UserDto implements Serializable{
+@Getter
+@Setter
+public class UserDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	private String id;
@@ -23,119 +27,9 @@ public class UserDto implements Serializable{
 	private Byte status;
 	private Long sequence;
 	private String createTimeStr;
-
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getRoleId() {
-		return roleId;
-	}
-
-	public void setRoleId(String roleId) {
-		this.roleId = roleId;
-	}
-
-	public String getTrueName() {
-		return trueName;
-	}
-
-	public void setTrueName(String trueName) {
-		this.trueName = trueName;
-	}
-
-	public String getRoleName() {
-		return roleName;
-	}
-
-	public void setRoleName(String roleName) {
-		this.roleName = roleName;
-	}
-
-	public String getAuth() {
-		return auth;
-	}
-
-	public void setAuth(String auth) {
-		this.auth = auth;
-	}
-
-	public String getAuthName() {
-		return authName;
-	}
-
-	public void setAuthName(String authName) {
-		this.authName = authName;
-	}
-
-	public Byte getType() {
-		return type;
-	}
-
-	public void setType(Byte type) {
-		this.type = type;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getAvatarUrl() {
-		return avatarUrl;
-	}
-
-	public void setAvatarUrl(String avatarUrl) {
-		this.avatarUrl = avatarUrl;
-	}
-
-	public Integer getLoginType() {
-		return loginType;
-	}
-
-	public void setLoginType(Integer loginType) {
-		this.loginType = loginType;
-	}
-
-	public String getThirdlyId() {
-		return thirdlyId;
-	}
-
-	public void setThirdlyId(String thirdlyId) {
-		this.thirdlyId = thirdlyId;
-	}
-
-	public Byte getStatus() {
-		return status;
-	}
-
-	public void setStatus(Byte status) {
-		this.status = status;
-	}
-
-	public Long getSequence() {
-		return sequence;
-	}
-
-	public void setSequence(Long sequence) {
-		this.sequence = sequence;
-	}
+    private String attributes;
+    private String attrKey;
+    private String attrVal;
 
 	public String getTypeName(){
 		if (type == null){
@@ -144,11 +38,4 @@ public class UserDto implements Serializable{
 		return UserType.getNameByValue(type);
 	}
 
-	public String getCreateTimeStr() {
-		return createTimeStr;
-	}
-
-	public void setCreateTimeStr(String createTimeStr) {
-		this.createTimeStr = createTimeStr;
-	}
 }

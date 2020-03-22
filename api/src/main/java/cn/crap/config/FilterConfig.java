cb4a13f9cc1d8a31e8338ce7e24e041c8c23514c
chengdu <3281328128@qq.com>
2020-03-22 22:28:09 +0800

    mvc 视图前缀、增加参数filter

--- api/src/main/java/cn/crap/config/FilterConfig.java
+++ api/src/main/java/cn/crap/config/FilterConfig.java
@@ -1,5 +1,6 @@
 package cn.crap.config;
 
+import cn.crap.framework.ParamFilter;
 import cn.crap.framework.ThreadContext;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -20,13 +21,27 @@ public class FilterConfig {
         }
     };
 
-    @Bean
+    @Bean(name = "threadContextFilter")
     public FilterRegistrationBean<ThreadContext> contextFilterRegistrationBean() {
         FilterRegistrationBean<ThreadContext> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new ThreadContext());
         filterRegistrationBean.addUrlPatterns("/*");
         filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         filterRegistrationBean.setInitParameters(initParameters);
+        return filterRegistrationBean;
+    }
+
+    private static String[] urlPatterns = {
+            "*.do",
+            "*.json",
+            "*.htm"
+    };
+
+    @Bean(name = "paramFilter")
+    public FilterRegistrationBean<ParamFilter> createParamFilter() {
+        FilterRegistrationBean<ParamFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new ParamFilter());
+        filterRegistrationBean.addUrlPatterns(urlPatterns);
         return filterRegistrationBean;
     }
 }

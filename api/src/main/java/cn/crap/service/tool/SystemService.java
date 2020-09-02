939976fbf24fbf45e769a5b3b171cac886e7e6a7
IhsanTang <IhsanTang@163.com>
2020-09-03 00:25:37 +0800

    延长第三方登陆时间

--- api/src/main/java/cn/crap/service/tool/SystemService.java
+++ api/src/main/java/cn/crap/service/tool/SystemService.java
@@ -37,8 +37,8 @@ public class SystemService {
     private final static String JS_FILE_URLS[] = new String[]{"app.js", "const.js", "services.js", "userRouter.js", "router.js",
             "userCtrls.js", "userBugCtrl.js", "userProjectMetaCtrl.js", "userCommonCtrl.js", "commentCtrl.js", "visitorControllers.js", "visitorRouter.js", "core.js", "coreNew.js", "global.js", "validateAndRefresh.js",
             "crapApi.js", "json.js", "editor.js"};
-    private final static String JS_COMPRESS_URL = "http://tool.oschina.net/action/jscompress/js_compress?munge=0&linebreakpos=5000";
-    private final static String CSS_COMPRESS_URL = "http://tool.oschina.net/action/jscompress/css_compress?linebreakpos=5000";
+    private final static String JS_COMPRESS_URL = "https://tool.oschina.net/action/jscompress/js_compress?munge=0&linebreakpos=5000";
+    private final static String CSS_COMPRESS_URL = "https://tool.oschina.net/action/jscompress/css_compress?linebreakpos=5000";
 
     private static final LinkedHashMap<Integer, String> CHANGE_SQL_MAP = Maps.newLinkedHashMap();
     static {
